60827bf0016db6a944b8bb853bb917a85c5d1fb5
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2011-05-29 22:25:09 +0100

    Compiling 1+2 works.

--- Python/compiler.py
+++ Python/compiler.py
@@ -1,16 +1,29 @@
 import ast
 
-
-
 class Codegen(ast.NodeVisitor):
-    
+
+    def __init__(self):
+        super().__init__()
+
+        self.pir = ""
+
     def generic_visit(self, node):
-        print(type(node).__name__)
+        #print(type(node).__name__)
 
         super().generic_visit(node)
 
-#    def visit_Load(
+    #def visit_Module(self, node):
+    #    pass #ignore all scope for now
+
+    def visit_Add(self, node):
+        self.pir += '+'
+
+    def visit_Num(self, node):
+        self.pir += str(node.n)
 
 if __name__ == '__main__':
     t = ast.parse('1+2')
-    Codegen().visit(t)
+    c = Codegen()
+    c.visit(t)
+
+    print(c.pir)
