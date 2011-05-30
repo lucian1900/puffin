93f9d70a5aaf3f76d2d4d31a5fc2d1395df814a9
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2011-05-30 01:02:15 +0100

    PIR generated for adding numbers works on parrot. Tests are needed next.

--- Python/compiler.py
+++ Python/compiler.py
@@ -8,22 +8,60 @@ class Codegen(ast.NodeVisitor):
         self.pir = ""
 
     def generic_visit(self, node):
-        #print(type(node).__name__)
+        print(type(node).__name__)
 
         super().generic_visit(node)
 
-    #def visit_Module(self, node):
-    #    pass #ignore all scope for now
+    def visit_Module(self, node):
+        self.pir += ".sub 'main' :main\n"
+        
+        super().generic_visit(node)
+
+        self.pir += ".end"
+
+    def visit_BinOp(self, node):
+        #nothing to do for now
+        super().generic_visit(node)
 
     def visit_Add(self, node):
-        self.pir += '+'
+        self.pir += ' + '
 
     def visit_Num(self, node):
         self.pir += str(node.n)
 
-if __name__ == '__main__':
-    t = ast.parse('1+2')
+    def visit_Expr(self, node):
+        super().generic_visit(node)
+
+        self.pir += ' '
+
+    def visit_Call(self, node):
+        if node.func.id == 'print':
+            self.pir += '\nsay '
+            ids = (i.id for i in node.args)
+            self.pir += ', '.join(ids)
+
+            self.pir += '\n'
+
+    def visit_Assign(self, node):
+        self.pir += '.local int {0}\n'.format(node.targets[0].id)
+        
+        super().generic_visit(node)
+
+    def visit_Name(self, node): 
+        self.pir += node.id
+
+        super().generic_visit(node)
+
+    def visit_Store(self, node):
+        self.pir += ' = '
+
+def pcompile(code):
+    t = ast.parse(code)
     c = Codegen()
     c.visit(t)
 
-    print(c.pir)
+    return c.pir
+
+if __name__ == '__main__':
+    print(pcompile('a = 1+2;print(a)'))
+
