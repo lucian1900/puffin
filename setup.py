2c80983cd760c96f4510f750420827ca54d26631
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2011-06-02 19:01:01 +0100

    Add distutils script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'puffin',
+    packages = ['Python'],
+    version = '0.0.0',
+    description = 'Python3 compiler for Parrot',
+    author = 'Lucian Branescu Mihaila',
+    author_email = 'lucian.branescu@gmail.com',
+    url = 'http://bitbucket.org/lucian1900/puffin',
+)
